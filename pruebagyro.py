895046c8a649ac575488d01be72a459e499ea1d9
Daniel Aparicio Vázquez <35073235+danielapariciovaquez@users.noreply.github.com>
2026-01-21 15:53:23 +0100

    Update pruebagyro.py

--- pruebagyro.py
+++ pruebagyro.py
@@ -1,17 +1,15 @@
 from mpu6050 import mpu6050
 import time
 
-sensor = mpu6050(0x68)  # usa 0x69 si AD0 está a 3.3V
+sensor = mpu6050(0x68, bus=2)  # <- aquí el cambio
 
 while True:
     accel = sensor.get_accel_data()
     gyro  = sensor.get_gyro_data()
     temp  = sensor.get_temp()
 
-    print("Accel (m/s^2):", accel)
-    print("Gyro  (deg/s):", gyro)
-    print("Temp  (°C):", temp)
+    print("Accel:", accel)
+    print("Gyro :", gyro)
+    print("Temp :", temp)
     print("----")
-
     time.sleep(0.5)
-
